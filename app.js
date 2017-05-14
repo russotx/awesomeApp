f56da7aa64cf3bed1e5a963d76b13e6306259aa4
Daniel Russo <Daniel@Daniels-MacBook-Pro.local>
2017-05-14 14:28:41 -0500

    reverting merge of models, DOES NOT UNDO THE REPO HISTORYgit revert -m 1 6778b8e374db05a7d0cedb4b86ffaa51135aed14 CANNOT MERGE MODELS BRANCH AGAIN WITHOUT REVERTING THIS REVERT.

--- app.js
+++ app.js
@@ -5,22 +5,17 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-
-var index = require('./routes/html-routes');
-var users = require('./routes/api-routes');
+var index = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
-// Setting up port and requiring models for syncing
-// var PORT = process.env.PORT || 8080;
-
 // view engine setup
-var exphbs = require("express-handlebars");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -30,7 +25,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-console.log("")
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
