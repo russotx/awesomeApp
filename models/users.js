ee037ab503373fd81aec4844300e5e70dd7cf4ba
Daniel Russo <Daniel@Daniels-MacBook-Pro.local>
2017-05-15 00:33:40 -0500

    updated user.js to include password auth

--- models/users.js
+++ models/users.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt-nodejs');
+
 module.exports = function(sequelize, DataTypes) {
   const User = sequelize.define("User", 
     {
@@ -24,6 +26,21 @@ module.exports = function(sequelize, DataTypes) {
       classMethods: {
         associate: function(models) {
           User.hasMany(models.UserRoadmap, { onDelete: "cascade" });
+        }
+      },
+      // Creating a custom method for our User model. This will check if an unhashed password entered by
+      // The user can be compared to the hashed password stored in our database
+      instanceMethods: {
+        validPassword: function(password) {
+          return bcrypt.compareSync(password, this.password);
+        }
+      },
+      // Hooks are automatic methods that run during various phases of the User Model lifecycle
+      // In this case, before a User is created, we will automatically hash their password
+      hooks: {
+        beforeCreate: function(user, options, cb) {
+          user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+          cb(null, options);
         }
       }
     });
